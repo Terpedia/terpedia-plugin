07990bc7274762d533f08204b5bfc897432bace7
GitHub Action <action@github.com>
2025-09-04 17:03:27 +0000

    Bump plugin version to 2.0.18

--- terpedia.php
+++ terpedia.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Terpedia
  * Description: Comprehensive terpene encyclopedia with AI agents and research tools
- * Version: 2.0.17
+ * Version: 2.0.18
  * Author: Terpedia Team
  */
 
